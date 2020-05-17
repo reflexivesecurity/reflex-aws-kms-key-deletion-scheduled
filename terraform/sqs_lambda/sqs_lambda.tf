333b861fa24f3913ba5c727962ed0db043afc3d9
Max Anderson <maxwell.c.anderson@gmail.com>
2020-05-17 11:04:43 -0700

    Multi-region update

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,27 +1,7 @@
-module "reflex_aws_kms_key_deletion_scheduled" {
-  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
-  rule_name        = "KMSKeyDeletionScheduled"
-  rule_description = "Reflex Rule for detecting and preventing KMS Key deletion."
-
-  event_pattern = <<PATTERN
-{
-  "source": [
-    "aws.kms"
-  ],
-  "detail-type": [
-    "AWS API Call via CloudTrail"
-  ],
-  "detail": {
-    "eventSource": [
-      "kms.amazonaws.com"
-    ],
-    "eventName": [
-      "ScheduleKeyDeletion"
-    ]
-  }
-}
-PATTERN
-
+module "sqs_lambda" {
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
+  cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
+  cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
   function_name   = "KMSKeyDeletionScheduled"
   source_code_dir = "${path.module}/source"
   handler         = "reflex_aws_kms_key_deletion_scheduled.lambda_handler"
