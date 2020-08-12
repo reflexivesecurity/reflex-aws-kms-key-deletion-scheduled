84730dd779c6d49c385b5f909b19e4eb73f97a3e
Max Anderson <maxwell.c.anderson@gmail.com>
2020-08-12 11:10:56 -0700

    Update to reflex-engine v2.0.0

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,11 +1,11 @@
 module "sqs_lambda" {
-  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v1.0.0"
+  source                    = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v2.0.0"
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name   = "KMSKeyDeletionScheduled"
-  source_code_dir = "${path.module}/../../source"
-  handler         = "reflex_aws_kms_key_deletion_scheduled.lambda_handler"
-  lambda_runtime  = "python3.7"
+  function_name             = "KMSKeyDeletionScheduled"
+  package_location          = var.package_location
+  handler                   = "reflex_aws_kms_key_deletion_scheduled.lambda_handler"
+  lambda_runtime            = "python3.7"
   environment_variable_map = {
     SNS_TOPIC = var.sns_topic_arn,
     MODE      = var.mode
